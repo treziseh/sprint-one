51e01aaaf4b4e1832386d0b86bcb0c5145d94ad9
Harry Trezise <103060492@student.swin.edu.au>
2021-09-08 08:37:46 +1000

    updated index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="PHP-SRePS index page">
+    <meta name="author" content="Nick, William, David, Harry">
+    <!-- <link rel="icon" href="images/ICON16.png" type="image/gif" sizes="16x16"> -->
+    <title>PHP-SRePS</title>
+
+    <!-- Custom styles for this page -->
+    <link href="styles/style-main.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php #include_once "nav.inc" ?>
+
+    <?php # include_once "footer.inc" ?>
+  </body>
+</html>
