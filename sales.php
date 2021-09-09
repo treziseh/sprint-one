a5bb1ff8b05a6ab4dd3dff18b7e77edaa395ed3b
Nicholas Mueller <nicholas.mark.m@gmail.com>
2021-09-09 20:04:22 +1000

    Test

--- sales.php
+++ sales.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="PHP-SRePS Login">
     <meta name="author" content="Nick, William, David, Harry">
-    <meta name="version" content="Version 2"> <!-- Delete This line later --> 
+    <meta name="version" content="Version 3"> <!-- Delete This line later --> 
     <!-- <link rel="icon" href="images/ICON16.png" type="image/gif" sizes="16x16"> -->
     <title>Sales</title>
 
@@ -14,24 +14,6 @@
   </head>
   <body>
     <?php include_once "sidebar.inc" ?>
-    <!--
-    <?php
-    function sql_store_sale() {
-        require_once ("settings.php");
-        $conn = @mysqli_connect($host,
-        $user,
-        $pwd,
-        $sql_db
-        );
-        
-        if (!conn) {
-            echo "<p>Database connection failure</p>";
-        } else {
-            echo "<p>Database connection Successful</p>";
-        }
-    }
-    sql_store_sale();
-    ?> -->
 
     <?php # include_once "footer.inc" ?>
   </body>
