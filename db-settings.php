55091f1dd23160025f2de753610888c3c933c236
Nicholas Mueller <nicholas.mark.m@gmail.com>
2021-09-09 21:53:47 +1000

    Final Database Fix + Cleanup

--- db-settings.php
+++ db-settings.php
@@ -1,6 +1,6 @@
 <?php
   // Store connection settings for database
-  $host = "sreps-main.database.windows.net";
+  $host = "tcp:sreps-main.database.windows.net,1433";
   $user = "admin.phpsreps";
   $pwd = "dycsaX-mopru3-modhyg";
   $sql_db = "sreps-database-main";
--- sales.php
+++ sales.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="PHP-SRePS Login">
     <meta name="author" content="Nick, William, David, Harry">
-    <meta name="version" content="Version 13"> <!-- Delete This line later -->
     <!-- <link rel="icon" href="images/ICON16.png" type="image/gif" sizes="16x16"> -->
     <title>Sales</title>
 
@@ -17,15 +16,10 @@
 
     <?php
     function sql_store_sale() {
-        /*require_once ("db-settings.php");
-        $conn = @mysqli_connect($host,
-        $user,
-        $pwd,
-        $sql_db
-        ); */
+        require_once ("db-settings.php");
 
-        $serverName = "tcp:sreps-main.database.windows.net,1433";
-        $connectionInfo = array("UID" => "admin.phpsreps", "pwd" => "dycsaX-mopru3-modhyg", "Database" => "sreps-database-main", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+        $serverName = $host;
+        $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
         $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
         if (!$conn) {
