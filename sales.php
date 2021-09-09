ba08c9955aacabe5443cc9526c2c382f23576afa
Nicholas Mueller <nicholas.mark.m@gmail.com>
2021-09-09 20:24:00 +1000

    Test

--- sales.php
+++ sales.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="PHP-SRePS Login">
     <meta name="author" content="Nick, William, David, Harry">
-    <meta name="version" content="Version 7"> <!-- Delete This line later --> 
+    <meta name="version" content="Version 8"> <!-- Delete This line later --> 
     <!-- <link rel="icon" href="images/ICON16.png" type="image/gif" sizes="16x16"> -->
     <title>Sales</title>
 
@@ -18,10 +18,9 @@
     <?php
     function sql_store_sale() {
         require_once ("db-settings.php");
-        $conn = @mysqli_connect($host,
+        $conn = @mssql_connect($host,
         $user,
         $pwd,
-        $sql_db
         );
         
         if (!$conn) {
