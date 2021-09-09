53591805126453e27904ea9d58a2417925140761
Nicholas Mueller <nicholas.mark.m@gmail.com>
2021-09-09 20:01:16 +1000

    Test

--- sales.php
+++ sales.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="PHP-SRePS Login">
     <meta name="author" content="Nick, William, David, Harry">
-    <meta name="version" content="Version 1"> <!-- Delete This line later --> 
+    <meta name="version" content="Version 2"> <!-- Delete This line later --> 
     <!-- <link rel="icon" href="images/ICON16.png" type="image/gif" sizes="16x16"> -->
     <title>Sales</title>
 
@@ -14,7 +14,7 @@
   </head>
   <body>
     <?php include_once "sidebar.inc" ?>
-    
+    <!--
     <?php
     function sql_store_sale() {
         require_once ("settings.php");
@@ -31,7 +31,7 @@
         }
     }
     sql_store_sale();
-    ?> 
+    ?> -->
 
     <?php # include_once "footer.inc" ?>
   </body>
