c77527ca76af5207f3af916801d99e76c1845588
Nicholas Mueller <nicholas.mark.m@gmail.com>
2021-09-11 00:00:45 +1000

    Added Table Headers

--- sales.php
+++ sales.php
@@ -64,6 +64,7 @@
 
         echo "<form method='post' id='saleForm' action='sales.php'>";
         echo "<table>"; // start a table tag in the HTML
+        echo "<tr><th>Item Name</th><th>Base Price</th><th>RRP</th><th>MTD Sold</th><th>Exp Quant</th><th>Add</th></tr>";
         while($row = sqlsrv_fetch_array($result)){   //Creates a loop to loop through results
         echo "<tr><td>" . $row['item_name'] . "</td><td>" . $row['base_price'] . "</td><td>" . $row['rrp'] . "</td><td>" . $row['mtd_sold'] . "</td><td>" . $row['exp_quant'] . "</td>
         <td><input type='checkbox' id='" . $row['item_name'] . "' name='" . $row['item_name'] . "' value='true'></td></tr>";  //$row['index'] the index here is a field name
