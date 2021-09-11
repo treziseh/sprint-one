cf8985e0c9138f4f3f7112f16ac7df262cdc8093
William <83646897+Crystal-Meth@users.noreply.github.com>
2021-09-11 11:55:21 +1000

    Revert "Table Test 2"
    This reverts commit 0ee38a980cdeea4d100b0ae136b03d9f49d17ed2.

--- inventory.php
+++ inventory.php
@@ -13,50 +13,7 @@
   </head>
   <body>
     <?php include_once "sidebar.inc" ?>
-    <?php
-    function main() {
-        require_once ("db-settings.php");
-        $serverName = $host;
-        $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-        $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-        $query = "SELECT * FROM inventory";
-        $result = sqlsrv_query($conn, $query);
-        if ($result === false) { //Checks to see if query was passed
-                die( print_r( sqlsrv_errors(), true));
-        }
-
-        echo "<div class="col-xs-12">";
-        echo "<h1>Products</h1>";
-        echo "<br>";
-        echo "<table class="table table-bordered">";
-        echo "<thead>";
-        echo "<tr>";
-        echo "<th>Item Name</th>";
-        echo "<th>Base Price</th>";
-        echo "<th>RRP</th>";
-        echo "<th>MTD Sold</th>";
-        echo "<th>EXP Quantity</th>";
-        echo "</tr>";
-        echo "</thead>";
-        echo "<tbody>";
-
-        while($row = sqlsrv_fetch_array($result)){
-          echo "<tr>";
-          echo "<td>" . $row['item_name'] . "</td>";
-          echo "</tr>";
-        }
-
-        echo "</tbody>";
-        echo "</table>";
-        echo "</div>";
-
-        sqlsrv_close($conn);
-
-        validate(); //Calls validate function
-    }
-    main(); //Calls main function
-    ?>
     <?php # include_once "footer.inc" ?>
   </body>
 </html>
