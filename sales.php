1607d8476b964b6776da76095860018fa41888fe
Nicholas Mueller <nicholas.mark.m@gmail.com>
2021-09-11 18:28:16 +1000

    Fix Connection

--- sales.php
+++ sales.php
@@ -17,7 +17,6 @@
     <?php
     function sql_store_sale() { 
         require_once ("db-settings.php");
-
         $serverName = $host;
         $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
         $conn = sqlsrv_connect($serverName, $connectionInfo);
