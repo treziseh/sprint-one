17f731ea8943a6c2619447bc31c351fc7f5179d2
William <83646897+Crystal-Meth@users.noreply.github.com>
2021-09-11 18:54:28 +1000

    Testing Connection - New Product Page

--- newproduct.php
+++ newproduct.php
@@ -15,40 +15,52 @@
     <?php include_once "sidebar.inc" ?>
 
     <?php
-    echo "
-      <div class='col-xs-12'>
-      <h1>New Product</h1>
-      <form method='post'>
-      <label for='itemName'>Item Name:</label>
-      <br>
-      <textarea required id='itemName' name='itemName' cols='30' rows='5' class='form-control'></textarea>
-      <br>
-      <br>
-      <label for='basePrice'>Base Price:</label>
-      <br>
-      <input class='form-control' name='basePrice' required type='number' id='basePrice' placeholder='Enter base price'>
-      <br>
-      <br>
-      <label for='sellPrice'>RRP:</label>
-      <br>
-      <input class='form-control' name='sellPrice' required type='number' id='sellPrice' placeholder='Enter recommended retail price'>
-      <br>
-      <br>
-      <label for='mtd'>MTD Sold:</label>
-      <br>
-      <input class='form-control' name='mtd' required type='number' id='mtd' placeholder='Month to Date Sold'>
-      <br>
-      <br>
-      <label for='exp_quan'>EXP Quantity:</label>
-      <br>
-      <input class='form-control' name='exp_quan' required type='number' id='exp_quan' placeholder='Month to Date Sold'>
-      <br>
-      <br>
-      <input class='btn btn-info' type='submit' value='Add' name='add'>
+    function main() {
+      require ("db-settings.php");
+      $serverName = $host;
+      $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+      $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-      </form>
-      </div>
-    ";
+      $query = "SELECT * FROM inventory";
+      $result = sqlsrv_query($conn, $query);
+      if ($result === false) { //Checks to see if query was passed
+              die( print_r( sqlsrv_errors(), true));
+      }
+      echo "
+        <div class='col-xs-12'>
+        <h1>New Product</h1>
+        <form method='post'>
+        <label for='itemName'>Item Name:</label>
+        <br>
+        <textarea required id='itemName' name='itemName' cols='30' rows='5' class='form-control'></textarea>
+        <br>
+        <br>
+        <label for='basePrice'>Base Price:</label>
+        <br>
+        <input class='form-control' name='basePrice' required type='number' id='basePrice' placeholder='Enter base price'>
+        <br>
+        <br>
+        <label for='sellPrice'>RRP:</label>
+        <br>
+        <input class='form-control' name='sellPrice' required type='number' id='sellPrice' placeholder='Enter recommended retail price'>
+        <br>
+        <br>
+        <label for='mtd'>MTD Sold:</label>
+        <br>
+        <input class='form-control' name='mtd' required type='number' id='mtd' placeholder='Month to Date Sold'>
+        <br>
+        <br>
+        <label for='exp_quan'>EXP Quantity:</label>
+        <br>
+        <input class='form-control' name='exp_quan' required type='number' id='exp_quan' placeholder='Month to Date Sold'>
+        <br>
+        <br>
+        <input class='btn btn-info' type='submit' value='Add' name='add'>
+
+        </form>
+        </div>
+      ";
+    }
     ?>
 
     <?php # include_once "footer.inc" ?>
