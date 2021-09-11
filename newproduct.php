a33e3fe6959fb83af6ae9713d11e0d7e345b16a4
William <83646897+Crystal-Meth@users.noreply.github.com>
2021-09-11 13:19:37 +1000

    Testing new product page

--- newproduct.php
+++ newproduct.php
@@ -17,23 +17,11 @@
     <?php
     echo "
       <div class='col-xs-12'>
-      <h1>Nuevo producto</h1>
+      <h1>New Product</h1>
       <form method='post'>
       <label for="bcode">Barcode:</label>
       <input class='form-control' name='bcode' required type='text' id='bcode' placeholder='Type the product barcode'>
 
-      <label for='desc'>Description:</label>
-      <textarea required id='desc' name='desc' cols='30' rows='5' class='form-control'></textarea>
-
-      <label for='salePrice'>Sale Price:</label>
-      <input class='form-control' name='salePrice' required type='number' id='salePrice' placeholder='Enter sale price'>
-
-      <label for='purchasePrice'>Purchase Price:</label>
-      <input class='form-control' name='purchasePrice' required type='number' id='purchasePrice' placeholder='Enter purchase price'>
-
-      <label for='soh'>Stock on Hand:</label>
-      <input class='form-control' name='soh' required type='number' id='soh' placeholder='Enter stock on hand value'>
-      <br><br><input class='btn btn-info' type='submit' value='Add'>
       </form>
       </div>
     ";
