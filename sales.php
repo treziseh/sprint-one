ecf5cdc37448d246bae47039ad1996a4789b5858
Nicholas Mueller <nicholas.mark.m@gmail.com>
2021-09-11 18:32:09 +1000

    Connection Fix

--- sales.php
+++ sales.php
@@ -16,7 +16,7 @@
 
     <?php
     function sql_store_sale() { 
-        require_once ("db-settings.php");
+        require ("db-settings.php");
         $serverName = $host;
         $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
         $conn = sqlsrv_connect($serverName, $connectionInfo);
@@ -71,7 +71,7 @@
     }
 
     function main() {
-        require_once ("db-settings.php");
+        require ("db-settings.php");
         $serverName = $host;
         $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
         $conn = sqlsrv_connect($serverName, $connectionInfo);
