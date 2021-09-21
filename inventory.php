9704748234d313a8eeba822d36c13ad2112f7cd9
William <83646897+Crystal-Meth@users.noreply.github.com>
2021-09-21 20:05:18 +1000

    Debug

--- inventory.php
+++ inventory.php
@@ -63,7 +63,6 @@
           <td>" . $row['base_price'] . "</td>
           <td>" . $row['sale_price'] . "</td>
           <td>" . $row['soh'] . "</td>
-          <td><center><a class='btn btn-warning' href='<editproduct.php?id=" . $row['barcode'] . ">Edit <i class='fa fa-edit'></i></a></center></td>
           <td><center><a href='editproduct.php?id=" . $row['barcode'] . "' class="buttonize">Edit</a></center></td>
           </tr>
           </tbody>
