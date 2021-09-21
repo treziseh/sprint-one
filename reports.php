961c7202616fa93e28ee389eb8e6f2ccd5fcbedd
Harry Trezise <103060492@student.swin.edu.au>
2021-09-22 08:31:12 +1000

    Predict form

--- reports.php
+++ reports.php
@@ -92,6 +92,36 @@
         echo "</tbody></table>";
       }
     ?>
-    <?php # include_once "footer.inc" ?>
+    <form action="reports.php?<?php echo session_id(); ?>" method="post">
+      <fieldset>
+        <legend>Predict</legend>
+        <?php
+          require_once ("db-settings.php");
+          $serverName = $host;
+          $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+          $conn = sqlsrv_connect($serverName, $connectionInfo); //Create connection
+
+          $query = "SELECT barcode, item_name FROM inventory";
+          $result = sqlsrv_query($conn, $query);
+          if ($result === false) { //Checks to see if query was passed
+              die( print_r( sqlsrv_errors(), true));
+          }
+
+          while ($row = sqlsrv_fetch_array($result)) {
+            echo "<input type='checkbox' id='" . $row['barcode'] . "' name='" . $row['barcode'] . "' value='" . $row['barcode'] . "'><label for='" . $row['barcode'] . "'>" . $row['item_name'] . " | " . $row['barcode'] . "</label><br>";
+          }
+        ?>
+        <label for="timePeriod">Time period: </label>
+        <select name="timePeriod" id="timePeriod">
+          <option value="week">Week</option>
+          <option value="month">Month</option>
+        </select>
+        <label for="dateStarting">Date starting: </label>
+        <input type="date" id="dateStarting" name="dateStarting"><br>
+        <input type="hidden" name="reportPredict">
+        <input type="submit" value="Generate Report">
+      </fieldset>
+    </form>
+    <?php sqlsrv_close($conn); ?>
   </body>
 </html>
