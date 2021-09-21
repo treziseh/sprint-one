df357415b2908f666e5071cac35fa6e6e602d805
William <83646897+Crystal-Meth@users.noreply.github.com>
2021-09-21 18:16:55 +1000

    Updated NewProduct Page

--- newproduct.php
+++ newproduct.php
@@ -30,15 +30,15 @@
 
       //Grabbing data values from textboxes
       if(isset($_POST['add'])) {
+        $barcode = $_POST['barCode'];
         $item_name = $_POST['itemName'];
         $base_price = $_POST['basePrice'];
-        $rrp = $_POST['sellPrice'];
-        $mtd_sold = $_POST['mtd'];
-        $exp_quant = $_POST['exp_quan'];
+        $sale_price = $_POST['sellPrice'];
+        $soh = $_POST['SOH'];
 
         // Attempt insert query execution
-        $sql = "INSERT INTO inventory (item_name, base_price, rrp, mtd_sold, exp_quant)
-        VALUES ('$item_name', '$base_price', '$rrp', '$mtd_sold', '$exp_quant')";
+        $sql = "INSERT INTO inventory (barcode, item_name, base_price, sale_price, soh)
+        VALUES ('$barcode', '$item_name', '$base_price', '$sale_price', '$soh')";
         if (sqlsrv_query($conn, $sql)){
           echo "New record created sucessfully!";
         }
@@ -85,6 +85,11 @@
         <div class='col-xs-12'>
         <h1>New Product</h1>
         <form method='post'>
+        <label for='barCode'>Barcode:</label>
+        <br>
+        <input class='form-control' name='barCode' required type='number' id='barCode' placeholder='Enter product barcode'>
+        <br>
+        <br>
         <label for='itemName'>Item Name:</label>
         <br>
         <textarea required id='itemName' name='itemName' cols='30' rows='5' class='form-control'></textarea>
@@ -92,22 +97,17 @@
         <br>
         <label for='basePrice'>Base Price:</label>
         <br>
-        <input class='form-control' name='basePrice' required type='number' id='basePrice' placeholder='Enter base price'>
+        <input class='form-control' name='basePrice' required type='number' id='basePrice' placeholder='Enter pruchase price'>
         <br>
         <br>
-        <label for='sellPrice'>RRP:</label>
+        <label for='sellPrice'>Sell Price:</label>
         <br>
-        <input class='form-control' name='sellPrice' required type='number' id='sellPrice' placeholder='Enter recommended retail price'>
-        <br>
-        <br>
-        <label for='mtd'>MTD Sold:</label>
-        <br>
-        <input class='form-control' name='mtd' required type='number' id='mtd' placeholder='Month to Date Sold'>
+        <input class='form-control' name='sellPrice' required type='number' id='sellPrice' placeholder='Enter sale price'>
         <br>
         <br>
-        <label for='exp_quan'>EXP Quantity:</label>
+        <label for='SOH'>Stock on Hand (SOH):</label>
         <br>
-        <input class='form-control' name='exp_quan' required type='number' id='exp_quan' placeholder='Month to Date Sold'>
+        <input class='form-control' name='SOH' required type='number' id='SOH' placeholder='Stock on Hand'>
         <br>
         <br>
         <input class='btn btn-info' type='submit' value='Add' name='add'>
