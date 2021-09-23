167193cc14c793bd78ffbd8f8384e12a97fda4d9
William <83646897+Crystal-Meth@users.noreply.github.com>
2021-09-23 12:27:42 +1000

    Debug

--- editproduct.php
+++ editproduct.php
@@ -82,7 +82,7 @@
       }
 
       echo "
-        <form method='post' id='newProduct' action='newproduct.php?" . session_id() . "'>
+        <form method='post' id='editProduct' action='editproduct.php?" . session_id() . "'>
         <div class='col-xs-12'>
         <h1>Edit Product</h1>
         <form method='post'>
