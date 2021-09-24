4589bb9d665ec8870978c18798f68c051783921c
Nicholas Mueller <nicholas.mark.m@gmail.com>
2021-09-25 00:52:25 +1000

    Debug

--- saleshistory.php
+++ saleshistory.php
@@ -63,8 +63,8 @@
             ";
         while($row = sqlsrv_fetch_array($result)) {   //Creates a loop to loop through results
             /*$itemname = $row['item_name'];
-            $inventoryquery = "SELECT * FROM inventory WHERE item_name = $itemname";
-            $inventoryresult = sqlsrv_query($conn, $inventoryquery);*/
+            $inventoryquery = "SELECT * FROM inventory WHERE item_name = $itemname";*/
+            $inventoryresult = sqlsrv_query($conn, $inventoryquery);
             if ($inventoryresult === false) { //Checks to see if query was passed
                 die( print_r( sqlsrv_errors(), true));
             }
