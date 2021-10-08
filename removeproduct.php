3123a68c004f562855fe2225173875d6f8c8cec0
William <83646897+Crystal-Meth@users.noreply.github.com>
2021-10-08 16:08:53 +1100

    Debug

--- removeproduct.php
+++ removeproduct.php
@@ -1,6 +1,9 @@
 <?php
+  session_start();
   if(!isset($_GET["barcode"])) exit();
+?>
 
+<?php
   require ("db-settings.php");
   $serverName = $host;
   $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
