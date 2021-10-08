599a5620e5577abbaa9a0c10da6107a7983f7497
William <83646897+Crystal-Meth@users.noreply.github.com>
2021-10-08 15:38:29 +1100

    Testing remove product

---
+++ removeproduct.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+  if (!isset($_SESSION['username'])) {
+    header("location: index.php");
+  }
+  if(!isset($_GET["barcode"])) exit();
+
+  require ("db-settings.php");
+  $serverName = $host;
+  $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+  $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+  if (isset($_GET["barcode"]) {
+    $barcode = $_GET["barcode"];
+  } else if (isset($_POST["barcode"]) {
+    $barcode = $_POST["barcode"];
+  }
+  //$barcode = $_GET["barcode"];
+  $query = "DELETE FROM inventory WHERE barcode = $barcode";
+  $result = sqlsrv_query($conn, $query);
+  if ($result === false) { //Checks to see if query was passed
+          die( print_r( sqlsrv_errors(), true));
+  }
+  while($row = sqlsrv_fetch_array($result)){
+    header("Location: ./inventory.php");
+	  exit;
+  }
+?>
