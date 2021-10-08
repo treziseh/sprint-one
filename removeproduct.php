8fe5d6d0e420b97ec713fb2651a74c991a449a7e
William <83646897+Crystal-Meth@users.noreply.github.com>
2021-10-08 16:58:01 +1100

    removeProduct - Removal of validation

--- removeproduct.php
+++ removeproduct.php
@@ -9,19 +9,9 @@
   $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
   $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-  //Checking connection - DELETE LATER
-  if (!$conn) {
-      echo "<p>Failed</p>";
-      die( print_r( sqlsrv_errors(), true));
-  } else {
-      echo "<p>Database connection Successful</p>";
-  }
-
   if (isset($_GET['barcode'])) {
     $barcode = $_GET['barcode'];
   }
-
-  echo "$barcode"; //Testing - DELETE AFTER
 
   $query = "DELETE FROM inventory WHERE barcode = $barcode";
   $result = sqlsrv_query($conn, $query);
