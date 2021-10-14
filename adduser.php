991e3bdc5ec6ad9abc9b5ca2a5f1fb39f1e2ace9
Harry Trezise <103060492@student.swin.edu.au>
2021-10-14 17:34:42 +1100

    Debug

--- adduser.php
+++ adduser.php
@@ -16,6 +16,9 @@
   }
 
   require_once ("db-settings.php");
+  $serverName = $host;
+  $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+  $conn = sqlsrv_connect($serverName, $connectionInfo); //Create connection
 
   $query = "SELECT uname FROM login";
   $result = sqlsrv_query($conn, $query);
@@ -32,9 +35,6 @@
   }
 
   if ($err == "") {
-    $serverName = $host;
-    $connectionInfo = array("UID" => $user, "pwd" => $pwd, "Database" => $sql_db, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $conn = sqlsrv_connect($serverName, $connectionInfo); //Create connection
 
     $query = "INSERT INTO login (uname, pword) VALUES ('$newUser', '$newPword')";
     $result = sqlsrv_query($conn, $query);
